66a55842736d547785eb9d1e93d6cf891082dea3
Yoyae <gingin.fr@gmail.com>
2018-01-22 11:56:02 +0100

    Monoecify

--- README.md
+++ README.md
@@ -1,42 +1,43 @@
-# Dash Ninja Back-End (dashninja-be)
-By Alexandre (aka elbereth) Devilliers
+# Monoeci Ninja Back-End (monoecininja-be)
 
-[![Codacy Badge](https://api.codacy.com/project/badge/Grade/f4a2d60364cd4c1cb34c81e23453f62a)](https://www.codacy.com/app/elbereth/dashninja-be?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=elbereth/dashninja-be&amp;utm_campaign=Badge_Grade)
-[![Code Climate](https://codeclimate.com/github/elbereth/dashninja-be/badges/gpa.svg)](https://codeclimate.com/github/elbereth/dashninja-be)
+Initial code by Alexandre (aka elbereth) Devilliers (https://github.com/elbereth/dashninja-be)
 
-Check the running live website at https://www.dashninja.pl
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/f4a2d60364cd4c1cb34c81e23453f62a)](https://www.codacy.com/app/Yoyae/monoecininja-be?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=Yoyae/monoecininja-be&amp;utm_campaign=Badge_Grade)
+[![Code Climate](https://codeclimate.com/github/Yoyae/monoecininja-be/badges/gpa.svg)](https://codeclimate.com/github/Yoyae/monoecininja-be)
 
-This is part of what makes the Dash Ninja monitoring application.
+Check the running live website at https://www.monoecininja.pl
+
+This is part of what makes the Monoeci Ninja monitoring application.
 It contains:
 - Private REST API (using PHP and Phalcon framework)
 
 ## Requirement:
-* You will need a running website, official at https://dashninja.pl uses nginx
+* You will need a running website, official at https://monoecininja.pl uses nginx
 
 For the REST API:
 * PHP v5.6 with mysqli
 * Phalcon v2.0.x (should work with any version)
-* MySQL database with Dash Ninja Database (check dashninja-db repository)
+* MySQL database with Monoeci Ninja Database (check monoecininja-db repository)
 
 ## Optional:
-* Dash Ninja Control script installed and running (to feed the database through this API)
+* Monoeci Ninja Control script installed and running (to feed the database through this API)
 
 ## Install:
-* Go to the root of your website for Dash private API (ex: cd /home/dashninja/cmd/)
+* Go to the root of your website for Monoeci private API (ex: cd /home/monoecininja/cmd/)
 * Get latest code from github:
 ```shell
-git clone https://github.com/elbereth/dashninja-be.git
+git clone https://github.com/Yoyae/monoecininja-be.git
 ```
 
 * Configure php to answer only to calls to api/index.php rewriting to end-point api/
 
 ## Configuration:
-The initial idea was to be able to have a central application (dashninja-be) where one or several hub of nodes connect to in order to provide monitoring.
+The initial idea was to be able to have a central application (monoecininja-be) where one or several hub of nodes connect to in order to provide monitoring.
 
 Current implementation takes as assumption that there is only one hub configured (there can be several nodes).
 
 Identification of this app and possible hubs are done with SSL certificates and it was designed to run on IPv6 only.
-You need to configure your web server (Dash Ninja official uses nginx) with your CA and create client certificates to trusted hubs to connect to it.
+You need to configure your web server (Monoeci Ninja official uses nginx) with your CA and create client certificates to trusted hubs to connect to it.
 
 Once the server is configured correctly import the client certificate CN into cmd_hub to give access to that hub (this can be a remote host or ::1). Both the certificate and IPv6 are checked to match and give access.
 
--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <?php
 
 /*
-    This file is part of Dash Ninja.
-    https://github.com/elbereth/dashninja-be
+    This file is part of Monoeci Ninja.
+    https://github.com/Yoyae/monoecininja-be
 
-    Dash Ninja is free software: you can redistribute it and/or modify
+    Monoeci Ninja is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Dash Ninja is distributed in the hope that it will be useful,
+    Monoeci Ninja is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Dash Ninja.  If not, see <http://www.gnu.org/licenses/>.
+    along with Monoeci Ninja.  If not, see <http://www.gnu.org/licenses/>.
 
  */
 
 /*****************************************************************************
- * Dash Ninja Back-end Private REST API                                      *
+ * Monoeci Ninja Back-end Private REST API                                      *
  *---------------------------------------------------------------------------*
  * This script is the backend interface between hubs                         *
  * It is the foundation for all other scripts, it is private API and is not  *
@@ -77,7 +77,7 @@ $eventManager->attach('micro', function($event, $app) use ($mysqli) {
     // Now we need to check the peer is a known/allowed hub (via its client certificate and the remote address)
     $cacheserial = sha1($_SERVER['DN']);
     $cacheserial2 = sha1($_SERVER['REMOTE_ADDR']);
-    $cachefnam = CACHEFOLDER.sprintf("dashninja_cmd_hubcheck_%s_%s",$cacheserial,$cacheserial2);
+    $cachefnam = CACHEFOLDER.sprintf("monoecininja_cmd_hubcheck_%s_%s",$cacheserial,$cacheserial2);
     $cachevalid = (is_readable($cachefnam) && ((filemtime($cachefnam)+7200)>=time()));
     if ($cachevalid) {
       $data = unserialize(file_get_contents($cachefnam));
@@ -381,7 +381,7 @@ $app->post('/blocks', function() use ($app,&$mysqli) {
   }
   else {
     // Retrieve all known nodes for current hub
-    $result = dashninja_cmd_getnodes($mysqli,$authinfo['HubId'],0);
+    $result = monoecininja_cmd_getnodes($mysqli,$authinfo['HubId'],0);
     $numnodes = 0;
     $nodes = array();
     if (count($result) > 0) {
@@ -390,7 +390,7 @@ $app->post('/blocks', function() use ($app,&$mysqli) {
         $nodes[$nodename] = $row['NodeId'];
       }
     }
-    $result = dashninja_cmd_getnodes($mysqli,$authinfo['HubId'],1);
+    $result = monoecininja_cmd_getnodes($mysqli,$authinfo['HubId'],1);
     if (count($result) > 0) {
         foreach($result as $nodename => $row){
             $numnodes++;
@@ -440,10 +440,10 @@ $app->post('/blocks', function() use ($app,&$mysqli) {
           return $response;
         }
         if ($curratio[0] > -1) {
-          $stats[] = sprintf("('mnpaymentratio','%s',%d,'dashninja')",$curratio[0],time());
+          $stats[] = sprintf("('mnpaymentratio','%s',%d,'monoecininja')",$curratio[0],time());
         }
         if ($curratio[1] > -1) {
-          $stats[] = sprintf("('mnpaymentratiotest','%s',%d,'dashninja')",$curratio[1],time());
+          $stats[] = sprintf("('mnpaymentratiotest','%s',%d,'monoecininja')",$curratio[1],time());
         }
       }
 
@@ -616,17 +616,17 @@ EOT;
             if ($row["BlockTestNet"] == 1) {
               $statkey .= "test";
             }
-            $stats[] = sprintf("('%s','%s',%d,'dashninja')",$statkey,$row["TotalSupplyValue"],time());
+            $stats[] = sprintf("('%s','%s',%d,'monoecininja')",$statkey,$row["TotalSupplyValue"],time());
             $statkey = "paymentdrk";
             if ($row["BlockTestNet"] == 1) {
               $statkey .= "test";
             }
-            $stats[] = sprintf("('%s','%s',%d,'dashninja')",$statkey,$row["TotalMNValue"],time());
+            $stats[] = sprintf("('%s','%s',%d,'monoecininja')",$statkey,$row["TotalMNValue"],time());
             $statkey = "mnpayments";
             if ($row["BlockTestNet"] == 1) {
               $statkey .= "test";
             }
-            $stats[] = sprintf("('%s','%s',%d,'dashninja')",$statkey,round(($row["NumPayed"]/$row["NumBlocks"])*100,2),time());
+            $stats[] = sprintf("('%s','%s',%d,'monoecininja')",$statkey,round(($row["NumPayed"]/$row["NumBlocks"])*100,2),time());
           }
         }
       }
@@ -826,7 +826,7 @@ $app->get('/superblocksexpected', function() use ($app,&$mysqli) {
 });
 
 // Function to retrieve the masternode list
-function dashninja_masternodes_get($mysqli, $testnet = 0, $protocol = 0) {
+function monoecininja_masternodes_get($mysqli, $testnet = 0, $protocol = 0) {
 
   $sqlmaxprotocol = sprintf("SELECT MAX(NodeProtocol) Protocol FROM cmd_nodes cn, cmd_nodes_status cns WHERE cn.NodeId = cns.NodeId AND NodeTestnet = %d GROUP BY NodeTestnet",$testnet);
   // Run the query
@@ -1268,9 +1268,9 @@ $app->get('/nodes', function() use ($app,&$mysqli) {
 
 });
 
-function dashninja_cmd_getnodes($mysqli,$hubid = -1,$testnet = 0) {
+function monoecininja_cmd_getnodes($mysqli,$hubid = -1,$testnet = 0) {
 
-  $cachefnam = CACHEFOLDER.sprintf("dashninja_cmd_getnodes_%d_%d",$hubid,$testnet);
+  $cachefnam = CACHEFOLDER.sprintf("monoecininja_cmd_getnodes_%d_%d",$hubid,$testnet);
   $cachevalid = (is_readable($cachefnam) && ((filemtime($cachefnam)+3600)>=time()));
   if ($cachevalid) {
     $nodes = unserialize(file_get_contents($cachefnam));
@@ -1343,7 +1343,7 @@ $app->post('/ping', function() use ($app,&$mysqli) {
   else {
     // Retrieve all known nodes for current hub
     $istestnet = intval($payload['testnet']);
-    $nodes = dashninja_cmd_getnodes($mysqli,$authinfo['HubId'],$istestnet);
+    $nodes = monoecininja_cmd_getnodes($mysqli,$authinfo['HubId'],$istestnet);
     $numnodes = count($nodes);
     if ($numnodes > 0) {
       if ($numnodes == count($payload['nodes'])) {
@@ -1841,8 +1841,8 @@ $app->post('/ping', function() use ($app,&$mysqli) {
           $activemncount = 0;
           $uniquemnips = 0;
           drkmn_masternodes_count($mysqli,$istestnet,$activemncount,$uniquemnips);
-          $sqlstats2[] = sprintf("('%s','%s',%d,'dashninja')",'mnactive',$activemncount,time());
-          $sqlstats2[] = sprintf("('%s','%s',%d,'dashninja')",'mnuniqiptest',$uniquemnips,time());
+          $sqlstats2[] = sprintf("('%s','%s',%d,'monoecininja')",'mnactive',$activemncount,time());
+          $sqlstats2[] = sprintf("('%s','%s',%d,'monoecininja')",'mnuniqiptest',$uniquemnips,time());
 
           $teststr = "";
           if ($istestnet == 1) {
@@ -1861,7 +1861,7 @@ $app->post('/ping', function() use ($app,&$mysqli) {
           $priceusd = $pricebtc*$tmp['usdbtc'];
           $activemncountath = $tmp["mnactiveath$teststr"];
           if ($activemncount > $activemncountath) {
-            $sqlstats2[] = sprintf("('%s','%s',%d,'dashninja')","mnactiveath$teststr",$activemncount,time());
+            $sqlstats2[] = sprintf("('%s','%s',%d,'monoecininja')","mnactiveath$teststr",$activemncount,time());
           }
 
           $sqlstats[] = sprintf("(%d,NOW(),%d,%d,%01.9f,%01.9f,%01.9f)",
@@ -1876,20 +1876,20 @@ $app->post('/ping', function() use ($app,&$mysqli) {
           if ($istestnet == 1) {
             $statkey .= "test";
           }
-          $sqlstats2[] = sprintf("('%s','%s',%d,'dashninja')",$statkey,$networkhashps,time());
+          $sqlstats2[] = sprintf("('%s','%s',%d,'monoecininja')",$statkey,$networkhashps,time());
           if ((isset($governancenextsuperblock)) && (!is_null($governancenextsuperblock)) && ($governancenextsuperblock > 0)) {
             $statkey = "governancesb";
             if ($istestnet == 1) {
               $statkey .= "test";
             }
-            $sqlstats2[] = sprintf("('%s','%s',%d,'dashninja')",$statkey,$governancenextsuperblock,time());
+            $sqlstats2[] = sprintf("('%s','%s',%d,'monoecininja')",$statkey,$governancenextsuperblock,time());
           }
           if ((isset($governancebudget)) && (!is_null($governancebudget)) && ($governancebudget > 0)) {
             $statkey = "governancebudget";
             if ($istestnet == 1) {
               $statkey .= "test";
             }
-            $sqlstats2[] = sprintf("('%s','%s',%01.9f,'dashninja')", $statkey, $governancebudget, time());
+            $sqlstats2[] = sprintf("('%s','%s',%01.9f,'monoecininja')", $statkey, $governancebudget, time());
           }
 
           $statsinfo = false;
@@ -2386,7 +2386,7 @@ $app->get('/portcheck/config', function() use ($app,&$mysqli) {
     $response->setJsonContent(array('status' => 'ERROR', 'messages' => 'Payload (or CONTENT_LENGTH) is missing'));
   }
   else {
-    $cachefnam = CACHEFOLDER."dashninja_cmd_portcheck_config";
+    $cachefnam = CACHEFOLDER."monoecininja_cmd_portcheck_config";
     $cachevalid = (is_readable($cachefnam) && ((filemtime($cachefnam)+7200)>=time()));
     if ($cachevalid) {
       $config = unserialize(file_get_contents($cachefnam));
@@ -2558,7 +2558,7 @@ $app->post('/portcheck', function() use ($app,&$mysqli) {
 //   POST
 // Parameters (JSON body):
 //   thirdparties=array of keys/values (mandatory)
-//   dashwhale=array of keys/values (mandatory)
+//   monoeciwhale=array of keys/values (mandatory)
 // Result (JSON body):
 //   status=OK|ERROR
 //   messages=array of error messages (only if status is ERROR)
@@ -2577,7 +2577,7 @@ $app->post('/thirdparties', function() use ($app,&$mysqli) {
   if (!array_key_exists('CONTENT_LENGTH',$_SERVER) || (intval($_SERVER['CONTENT_LENGTH']) == 0)
       || !is_array($payload) || (count($payload) == 0)
       || !array_key_exists("thirdparties",$payload) || !is_array($payload["thirdparties"])
-      || !array_key_exists("dashwhale",$payload) || !is_array($payload["dashwhale"])) {
+      || !array_key_exists("monoeciwhale",$payload) || !is_array($payload["monoeciwhale"])) {
     //Change the HTTP status
     $response->setStatusCode(400, "Bad Request");
 
@@ -2615,9 +2615,9 @@ $app->post('/thirdparties', function() use ($app,&$mysqli) {
       }
     }
 
-    // Dash Whale data
+    // Monoeci Whale data
     $sqldwc = array();
-    foreach($payload["dashwhale"] as $proposal) {
+    foreach($payload["monoeciwhale"] as $proposal) {
       $dwinfo = var_export($proposal,true);
       if (is_array($proposal) && (count($proposal) == 2)
       && array_key_exists("proposal",$proposal) && is_array($proposal["proposal"])
@@ -2656,7 +2656,7 @@ $app->post('/thirdparties', function() use ($app,&$mysqli) {
     }
 
       if (count($sqldwc) > 0) {
-          $sql = "INSERT INTO cmd_budget_dashwhale_comments (BudgetHash, CommentHash, CommentUsername, CommentDate, "
+          $sql = "INSERT INTO cmd_budget_monoeciwhale_comments (BudgetHash, CommentHash, CommentUsername, CommentDate, "
                 ."CommentOrder, CommentLevel, CommentRecentPost, CommentByOwner, CommentReplyURL, CommentContent)"
                 ." VALUES ".implode(',',$sqldwc)
                 ." ON DUPLICATE KEY UPDATE CommentUsername = VALUES(CommentUsername), CommentDate = VALUES(CommentDate), "
@@ -2681,7 +2681,7 @@ $app->post('/thirdparties', function() use ($app,&$mysqli) {
     if (count($errors) == 0) {
         $response->setStatusCode(202, "Accepted");
         $response->setJsonContent(array('status' => 'OK', 'data' => array('thirdparties' => $statsinfo,
-                                                                          'dashwhale' => $dwinfo)));
+                                                                          'monoeciwhale' => $dwinfo)));
     }
     else {
         $response->setStatusCode(503, "Service Unavailable");
@@ -2696,7 +2696,7 @@ $app->post('/thirdparties', function() use ($app,&$mysqli) {
 // ============================================================================
 // VERSIONS
 // ----------------------------------------------------------------------------
-// End-point for creating new version of dashd to use by the nodes
+// End-point for creating new version of monoecid to use by the nodes
 // HTTP method:
 //   POST
 // Parameters (JSON body):
